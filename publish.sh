70fc0372ec2c02b6602128acb883c519e85afd36
Maciej Grela <enki@fsck.pl>
2021-04-25 17:45:43 +0200

    rebuild

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 emacs --batch -l publish.el --eval '(org-publish "magical-index")'
-(cd _site; git add .; git commit -m 'rebuild'; git push)
+if [ "$1" = "--push" ]; then
+	(cd _site; git add .; git commit -m 'rebuild'; git push)
+fi
