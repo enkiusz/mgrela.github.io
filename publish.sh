1166b8095d2cf11298a28bb127af110d93942f09
Maciej Grela <enki@fsck.pl>
2021-10-04 11:07:39 +0200

    rebuild

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-emacs --batch -l publish.el --eval '(org-publish "magical-index")'
+emacs -l publish.el --eval '(org-publish "magical-index")' --eval '(kill-emacs)'
 if [ "$1" = "--push" ]; then
 	(cd _site; git add .; git commit -m 'rebuild'; git push)
 fi
