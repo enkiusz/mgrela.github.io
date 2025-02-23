393fd664ce2ae16396b3fdac3eeec08c420e2d1d
Maciej Grela <enki@fsck.pl>
2025-02-23 19:09:36 +0100

    rebuild

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ if [ "$1" = "--push" ]; then
 	(cd _site; git pull)
 fi
 
-emacs -l publish.el --eval '(org-publish "magical-index")' --eval '(kill-emacs)'
+emacs -Q -l publish.el --eval '(org-publish "magical-index")' --eval '(kill-emacs)'
 echo 'pop.fsck.pl' > _site/CNAME
 if [ "$1" = "--push" ]; then
 	(cd _site; git add .; git commit -m 'rebuild'; git push)
